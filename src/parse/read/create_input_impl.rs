242f881fe9fea77457562b5e944be4ad21f1ca8f
Alex Rutar <alex@rutar.org>
2024-01-18 11:01:46 +0000

    more agressive inlining
    improves performance by ~20-30%

--- src/parse/read/create_input_impl.rs
+++ src/parse/read/create_input_impl.rs
@@ -28,41 +28,50 @@ macro_rules! input_read_impl {
         }
 
         impl<'r> Read<'r> for $name<'r> {
+            #[inline]
             fn peek(&self) -> Option<u8> {
                 let bytes = $convert(self.input);
                 bytes.first().copied()
             }
 
+            #[inline]
             fn discard(&mut self) {
                 self.input = &self.input[1..];
             }
 
+            #[inline]
             fn next_entry_or_eof(&mut self) -> bool {
                 let (tail, res) = next_entry_or_eof(self.input);
                 self.input = tail;
                 res
             }
 
+            #[inline]
             fn comment(&mut self) {
                 self.input = comment(self.input)
             }
 
+            #[inline]
             fn identifier_unicode(&mut self) -> Result<UnicodeIdentifier<'r>, ReadError> {
                 self.apply(identifier_unicode)
             }
 
+            #[inline]
             fn identifier_ascii(&mut self) -> Result<AsciiIdentifier<'r>, ReadError> {
                 self.apply(identifier_ascii)
             }
 
+            #[inline]
             fn balanced(&mut self) -> Result<Text<'r>, ReadError> {
                 Ok(Text::$var(Cow::Borrowed(self.apply(balanced)?)))
             }
 
+            #[inline]
             fn protected(&mut self, until: u8) -> Result<Text<'r>, ReadError> {
                 Ok(Text::$var(Cow::Borrowed(self.apply(protected(until))?)))
             }
 
+            #[inline]
             fn number(&mut self) -> Result<Text<'r>, ReadError> {
                 self.apply(number)
             }
